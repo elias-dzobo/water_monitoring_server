00ba6ebabff3e3a085658abfb51e46656b69070b
elias-dzobo <elias.dzobo@gmail.com>
2022-10-31 09:35:13 +0000

    new changes

--- post.php
+++ post.php
@@ -5,18 +5,23 @@
     $username = "root";
     $password = "";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
     $water_level = $_GET["water_level"];
     $status = $_GET["status"]; 
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     $sql = "INSERT INTO `levels` (`water_level`, `status`) VALUES ('$water_level', '$status')"; 
 
-    if (mysqli_query($conn, $sql)) {
-        echo "success";
+    $res = mysqli_query($conn, $sql);
+    
+    if ($res) {
+        echo "success"; 
     } else {
-        echo "failed"; 
+        echo "failed";
     }
+    
+    
 
     
 
